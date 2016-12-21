d3104c710f9bc431b31c3a02cb22ad5b6a660bcf
jeremy.granger <jeremy.granger@colorado.edu>
2016-12-21 13:02:39 -0700

    Broken first layer of security

--- nightmare.js
+++ nightmare.js
@@ -1,24 +1,31 @@
 // require the nightmare library
 var Nightmare = require('nightmare');
+var fs = require('fs');
 
 // create a new browser window
 var nightmare = Nightmare({ show: true });
 
 nightmare
     .goto('http://159.203.244.183:3000/')
-    .wait('p')
+    .wait('body')
 
     // evaluate code IN THE NIGHTMARE BROWSER
     .evaluate(function () {
 
         // pass data from the nightmare browser back to this express app
-        return document.querySelector('p').innerHTML;
+        var bodyHTML =  document.querySelector('body').innerHTML;
+        bodyHTML = bodyHTML.split('</script>')[1].toString();
+        bodyHTML.replace('<sp&#97;n>', '');
+        return bodyHTML;
+
+
     })
 
     // close the browser window
     .end()
     // the result passed into this function is the value we returned from evaluate
     .then(function (result) {
+        //fs.writeFileSync('results.txt', result);
         console.log(result)
     })
     .catch(function (error) {
